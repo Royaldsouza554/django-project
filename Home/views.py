7cbf191ae38ae049a1711b1e12c013dc800ea95f
Royal <royaldsouza2004@gmail.com>
2024-07-29 18:57:24 +0530

    latest commit

--- Home/views.py
+++ Home/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(req):
-    return render(req , 'Home/home.html')
+    return render(req , 'Home/home.html' ,{"page" : "home"})
 
 def about(req):
-    return render(req , "Home/about-us.html")
+    return render(req , "Home/about-us.html" , {"page" : "about-us"})
 
 def contact(req):
-    return render(req , "Home/contact-us.html")
+    return render(req , "Home/contact-us.html" , {"page" : "contact-us"})
